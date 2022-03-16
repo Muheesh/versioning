6500122f42023064121b46f0f3bdd445b42dfaab
Muheesh <muheeshas@gmail.com>
2022-03-16 12:48:13 +0530

    multiply

---
+++ src/com/harman/project/Subtraction.java
@@ -0,0 +1,11 @@
+package com.harman.project;
+
+public class Subtraction {
+    public static void main(String[] args) {
+        int x = 70;
+        int y = 30;
+        int z = x-y;
+        System.out.println(z);
+
+    }
+}
