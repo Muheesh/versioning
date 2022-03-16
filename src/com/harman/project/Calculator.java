b0276c30a5d0b132fdad60404ea377799a19059b
Muheesh <muheeshas@gmail.com>
2022-03-16 12:43:30 +0530

    calculator

---
+++ src/com/harman/project/Calculator.java
@@ -0,0 +1,10 @@
+package com.harman.project;
+
+public class Calculator {
+    public static void main(String[] args) {
+        int x = 5;
+        int y = 9;
+        int z= x+y;
+        System.out.println(z);
+    }
+}
