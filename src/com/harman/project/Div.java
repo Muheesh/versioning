8b174cf1d9e292aeeef8d8ef6cf6112dac890844
Muheesh <muheeshas@gmail.com>
2022-03-16 12:52:10 +0530

    division

---
+++ src/com/harman/project/Div.java
@@ -0,0 +1,10 @@
+package com.harman.project;
+
+public class Div {
+    public static void main(String[] args) {
+        int x = 15;
+        int y = 70;
+        int z = x/y;
+        System.out.println(z);
+    }
+}
